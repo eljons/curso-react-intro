f4bc47df6fd20627009c0a016b886f14bf5ab6be
eljons <ikerj2@gmail.com>
2023-08-15 15:14:53 -0600

    Agregar render props y render functions a TodoList

--- src/App/index.js
+++ src/App/index.js
@@ -45,9 +45,14 @@ function App() {
         error={error}
         loading={loading}
         searchedTodos={searchedTodos}
+        totalTodos={totalTodos}
+        searchText={searchValue}
         onError={()=><TodosError />}
         onLoading={()=><TodosLoading />}
         onEmptyTodos={()=><EmptyTodos />}
+        onEmptySearchResults={
+          (searchText)=> <p>No hay resultados para {searchText}</p>
+        }
         render={todo => (
           <TodoItem 
             key={todo.text} 
@@ -57,7 +62,17 @@ function App() {
             onDelete={() => deleteTodo(todo.text)}
           />
         )}
-      />
+        >
+        {/* {todo => (
+          <TodoItem 
+            key={todo.text} 
+            text={todo.text} 
+            completed={todo.completed} 
+            onComplete={() => completeTodo(todo.text)}
+            onDelete={() => deleteTodo(todo.text)}
+          />
+        )} */}
+      </TodoList>
       
       {/* <TodoList>
         {loading && <TodosLoading />}
@@ -74,7 +89,7 @@ function App() {
           />
         ))}
       </TodoList> */}
-      
+
       <CreateTodoButton setOpenModal={setOpenModal} />
 
       {openModal && (
--- src/TodoList/index.js
+++ src/TodoList/index.js
@@ -1,13 +1,17 @@
 import './TodoList.css';
 
 function TodoList(props) {
+  const renderFunc = props.children || props.render
   return (
     <section className='TodoList--container'>
       {props.error && props.onError()}
       {props.loading && props.onLoading()}
-      {(!props.loading && props.searchedTodos.length === 0) && props.onEmptyTodos()}
 
-      {props.searchedTodos.map(props.render)}
+      {(!props.loading && props.totalTodos.length === 0) && props.onEmptyTodos()}
+
+      {(!!props.totalTodos && props.searchedTodos.length === 0) && props.onEmptySearchResults(props.searchText)}
+
+      {props.searchedTodos.map(renderFunc)}
       <ul>
         {props.children}
       </ul>
